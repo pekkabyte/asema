271214aba369ce639f96720933fee737877111fa
Pekka Sipilä <pekka.a.sipila@gmail.com>
2025-04-04 15:36:45 +0300

    Implement a barebones WebView with URL TextInput

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,37 @@
-import { Text, View } from "react-native";
+import { TextInput, Button } from "react-native";
+import { WebView } from 'react-native-webview';
+import Constants from 'expo-constants';
+import { StyleSheet } from 'react-native';
+import { useState } from 'react';
+import { StatusBar } from 'expo-status-bar';
 
 export default function Index() {
+  const [text, onChangeText] = useState("https://react.dev");
+  const [url, setUrl] = useState("https://react.dev");
+
+  function handleSubmit() {
+    setUrl(text)
+  }
+
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
-    </View>
+    <>
+      <StatusBar style="auto" backgroundColor="#000000" />
+      <WebView
+        style={styles.container}
+        source={{ uri: url }}
+      />
+      <TextInput
+        onChangeText={onChangeText}
+        onSubmitEditing={handleSubmit}
+        value={text}
+      />
+    </>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    marginTop: Constants.statusBarHeight,
+  },
+});
