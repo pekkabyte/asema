b0fb7b40e436f707376fe9725da8c745c49b58a6
Pekka Sipilä <pekka.a.sipila@gmail.com>
2025-05-27 16:03:41 +0300

    Add parse5 into the project for React Native DOM parsing

--- app/index.tsx
+++ app/index.tsx
@@ -1,9 +1,9 @@
-import { View, TextInput } from "react-native";
+import { TextInput } from "react-native";
 import { WebView } from 'react-native-webview';
 import { StyleSheet } from 'react-native';
 import { useState } from 'react';
-import { StatusBar } from 'expo-status-bar';
 import { SafeAreaView } from 'react-native-safe-area-context';
+const parse5 = require('parse5');
 
 export default function Index() {
   const [text, onChangeText] = useState("https://react.dev");
@@ -11,11 +11,14 @@ export default function Index() {
 
   function handleSubmit() {
     setUrl(text)
+
+    const document = parse5.parse('<!DOCTYPE html><html><head></head><body>Hi there!</body></html>');
+
+    console.log(document.childNodes[1].tagName); //> 'html'
   }
 
   return (
     <SafeAreaView style={styles.container}>
-      <StatusBar style="auto" backgroundColor="#000000" />
       <WebView
         source={{ uri: url }}
       />
