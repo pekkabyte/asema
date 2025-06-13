e45055bf807f10a3f70c4215cc92772f9038fb23
Pekka Sipilä <pekka.a.sipila@gmail.com>
2025-06-13 16:26:20 +0300

    Add function to retrieve elements by classname from parsed html document. Add photo to readme from pitching of the project at Startup Sauna

--- README.md
+++ README.md
@@ -5,3 +5,5 @@
 Platform browser is type of a web browser for interacting with multisided markets. Inspired by my Master's Thesis "Exploring business model for open digital platform from customer and operating system owner viewpoints" [https://urn.fi/URN:NBN:fi:aalto-202301291794](https://urn.fi/URN:NBN:fi:aalto-202301291794)
 
 [Platform browser](https://github.com/pekkabyte/platform-browser) repository will be for specification of the concept and this Asema repository for its implementation, named after the Finnish word for "station". As a platform browser can be seen as a railway station housing platform(s) that connect people on different sides of a market running on parallel tracks, so I figured the name would fit well.
+
+<img src="./assets/images/IMG-20250605-WA0030.jpg" width="100%" align="center"/>
--- app/index.tsx
+++ app/index.tsx
@@ -10,11 +10,43 @@ export default function Index() {
   const [url, setUrl] = useState("https://react.dev");
 
   function handleSubmit() {
-    setUrl(text)
+    setUrl(text);
+    fetch(url)
+      .then(response => response.text())
+      .then((html) => {
+        const document = parse5.parse(html);
+        console.log(getElementsByClass(document, "sp-code-editor"));
+      })
+  }
 
-    const document = parse5.parse('<!DOCTYPE html><html><head></head><body>Hi there!</body></html>');
+  function getElementsByClass(node: Node, className: string): Array<Element> {
+    interface Dictionary {
+      [key: string]: string;
+    }
 
-    console.log(document.childNodes[1].tagName); //> 'html'
+    var results = new Array<Element>();
+    for (let i=0; i<node.childNodes?.length; i++) {
+      var element = node.childNodes[i] as Element;
+      var attrs = new Array();
+      for (const [key, value] of Object.entries(element)) {
+        if (key == "attrs") {
+          attrs.push(value);
+        }
+      }
+      for (let j=0; j<attrs.length; j++) {
+        for (const [key, value] of Object.entries(attrs[j])) {
+          const dict = value as Dictionary;
+          if (dict.name == "class" && dict.value.includes(className)) {
+            results.push(element);
+          }
+        }
+      }
+      const result = getElementsByClass(node.childNodes[i], className);
+      if (result.length > 0) {
+        results = results.concat(result);
+      }
+    }
+    return results;
   }
 
   return (
